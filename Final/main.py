9547a5ec76f78618f80a001c1b1a3b630b5d9589
Kanapat <o.kanapat@gmail.com>
2023-11-20 17:24:10 +0700

    UpdateMainCode

--- Final/main.py
+++ Final/main.py
@@ -1,74 +1,108 @@
 import cv2
-import numpy as np
+import mediapipe as mp
 
-# Function to detect changes in the keyboard region
-def detect_typing(frame, prev_frame, keyboard_region):
-    # Convert frames to grayscale for difference calculation
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    prev_gray_frame = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+# Function to detect key presses on the virtual keyboard
+def detect_key_press(key, typed_text):
+    global keyboard_layout
 
-    # Calculate absolute difference between frames
-    diff = cv2.absdiff(gray_frame, prev_gray_frame)
+    if key in keyboard_layout:
+        if key == 'Backspace' and len(typed_text) > 0:
+            typed_text = typed_text[:-1]  # Remove the last character
+        elif key != 'Backspace':
+            typed_text += key
+        print(f"Typed: {typed_text}")
 
-    # Apply threshold to focus on significant changes
-    _, threshold_diff = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)
 
-    # Use the keyboard region to identify typing
-    typing_roi = threshold_diff[keyboard_region[1]:keyboard_region[3], keyboard_region[0]:keyboard_region[2]]
+# Function to process webcam frames and detect the keyboard and hand
+def process_webcam():
+    global typed_text, keyboard_layout
 
-    # Count non-zero pixels (changed pixels) in the region
-    change_count = cv2.countNonZero(typing_roi)
+    # Set up hand tracking
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands()
 
-    return change_count > 100  # Adjust this threshold based on your setup
-
-# Main function
-def main():
-    # Set up video capture
+    # Open the webcam
     cap = cv2.VideoCapture(0)
 
-    # Define the keyboard region (adjust these values based on your setup)
-    keyboard_region = (100, 200, 900, 500)
-
-    # Create a window for real-time camera feed
-    cv2.namedWindow('Real-time Camera Feed', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('Real-time Camera Feed', 800, 600)
-
-    # Create a window for displaying typed characters
-    cv2.namedWindow('Typed Characters', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('Typed Characters', 400, 200)
-
-    # Read the first frame
-    ret, prev_frame = cap.read()
-
-    # Initialize an empty string to store typed characters
-    typed_text = ""
-
     while True:
-        # Read a frame from the webcam
+        # Capture frame from the webcam
         ret, frame = cap.read()
 
-        # Detect typing on the simulated keyboard
-        typing_detected = detect_typing(frame, prev_frame, keyboard_region)
+        # Resize the frame for better processing speed
+        frame = cv2.resize(frame, (800, 600))
 
-        # Display the real-time camera feed
-        cv2.imshow('Real-time Camera Feed', frame)
+        # Convert the frame to RGB
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        # Display typed characters
-        if typing_detected:
-            typed_text += "A"  # Replace this with the actual character detected
-            cv2.putText(frame, typed_text, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            cv2.imshow('Typed Characters', frame)
+        # Process the frame with hand tracking
+        results = hands.process(frame_rgb)
 
-        # Update the previous frame
-        prev_frame = frame.copy()
+        # Draw landmarks if hand is detected
+        if results.multi_hand_landmarks:
+            for landmarks in results.multi_hand_landmarks:
+                for point in landmarks.landmark:
+                    x, y = int(point.x * frame.shape[1]), int(point.y * frame.shape[0])
+                    cv2.circle(frame, (x, y), 5, (255, 0, 0), -1)
 
-        # Check if the windows should be closed
+        # Check for key presses
         key = cv2.waitKey(1)
         if key == 27:  # Esc key
             break
+        elif key != -1 and key != 255:  # Check if any key is pressed
+            key_char = chr(key).upper()  # Get the character representation of the key
+            detect_key_press(key_char, typed_text)
 
     # Release the webcam
     cap.release()
+    cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
-    main()
+    typed_text = ""
+
+    # Define the virtual keyboard layout
+    keyboard_layout = {
+        'Backspace': (888, 9, 120, 50),
+
+        'Q': (111, 76, 50, 50),
+        'W': (179, 76, 50, 50),
+        'E': (246, 76, 50, 50),
+        'R': (314, 76, 50, 50),
+        'T': (382, 76, 50, 50),
+        'Y': (449, 76, 50, 50),
+        'U': (517, 76, 50, 50),
+        'I': (585, 76, 50, 50),
+        'O': (652, 76, 50, 50),
+        'P': (720, 76, 50, 50),
+        '[': (787, 76, 50, 50),
+        ']': (855, 76, 50, 50),
+        '\\': (921, 76, 89, 50),
+
+        'A': (128, 143, 50, 50),
+        'S': (196, 143, 50, 50),
+        'D': (264, 143, 50, 50),
+        'F': (331, 143, 50, 50),
+        'G': (399, 143, 50, 50),
+        'H': (466, 143, 50, 50),
+        'J': (534, 143, 50, 50),
+        'K': (602, 143, 50, 50),
+        'L': (669, 143, 50, 50),
+        ';': (737, 143, 50, 50),
+        "'": (805, 143, 50, 50),
+
+        'Z': (162, 211, 50, 50),
+        'X': (229, 211, 50, 50),
+        'C': (297, 211, 50, 50),
+        'V': (365, 211, 50, 50),
+        'B': (432, 211, 50, 50),
+        'N': (500, 211, 50, 50),
+        'M': (568, 211, 50, 50),
+        ',': (635, 211, 50, 50),
+        '.': (703, 211, 50, 50),
+        '/': (770, 211, 50, 50),
+
+        'Space': (260, 278, 411, 50),
+    }
+
+    # Process webcam frames
+    process_webcam()
